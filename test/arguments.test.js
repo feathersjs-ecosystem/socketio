36d00170821e5934f1bbea1ef8bf7cbca169330f
kostyan <kozzztya@gmail.com>
2017-04-14 12:21:08 +0300

    Fix codeclimate

--- test/arguments.test.js
+++ test/arguments.test.js
@@ -1,12 +1,8 @@
-// import assert from 'assert';
 import feathers from 'feathers';
 import io from 'socket.io-client';
 import socketio from '../src';
 
-let port = 3000;
-function getUniquePort () {
-  return port++;
-}
+const createUniquePort = UniquePortCreator(3000);
 
 describe('Arguments', function () {
   const options = { path: '/ws/' };
@@ -15,103 +11,99 @@ describe('Arguments', function () {
   });
 
   describe('No arguments', function () {
-    const port = getUniquePort();
+    const port = createUniquePort();
     const app = feathers().configure(socketio());
     const socket = io('http://localhost:' + port);
 
     it('should be connected', (done) => {
-      let server = app.listen(port);
-      app.setup(server);
-      server.on('listening', () => {
+      serverListen(app, port, () => {
         socket.on('connect', done);
       });
     });
   });
 
   describe('Pass options', function () {
-    const port = getUniquePort();
+    const port = createUniquePort();
     const app = feathers().configure(socketio(options));
     const socket = io('http://localhost:' + port, options);
 
     it('should be connected', (done) => {
-      const server = app.listen(port);
-      app.setup(server);
-      server.on('listening', () => {
+      serverListen(app, port, () => {
         socket.on('connect', done);
       });
     });
   });
 
   describe('Pass callback', function () {
-    const port = getUniquePort();
+    const port = createUniquePort();
     const app = feathers().configure(socketio(callback));
     const socket = io('http://localhost:' + port);
 
     it('should be connected', (done) => {
-      const server = app.listen(port);
-      app.setup(server);
-      server.on('listening', () => {
+      serverListen(app, port, () => {
         socket.once('event', done);
       });
     });
   });
 
   describe('Pass options and callback', function () {
-    const port = getUniquePort();
+    const port = createUniquePort();
     const app = feathers().configure(socketio(options, callback));
     const socket = io('http://localhost:' + port, options);
 
     it('should be connected', (done) => {
-      const server = app.listen(port);
-      app.setup(server);
-      server.on('listening', () => {
+      serverListen(app, port, () => {
         socket.once('event', done);
       });
     });
   });
 
   describe('Pass port and options', function () {
-    const socketPort = getUniquePort();
-    const appPort = getUniquePort();
+    const socketPort = createUniquePort();
+    const appPort = createUniquePort();
     const app = feathers().configure(socketio(socketPort, options));
     const socket = io('http://localhost:' + socketPort, options);
 
     it('should be connected', (done) => {
-      const server = app.listen(appPort);
-      app.setup(server);
-      server.on('listening', () => {
+      serverListen(app, appPort, () => {
         socket.on('connect', done);
       });
     });
   });
 
   describe('Pass port and callback', function () {
-    const socketPort = getUniquePort();
-    const appPort = getUniquePort();
+    const socketPort = createUniquePort();
+    const appPort = createUniquePort();
     const app = feathers().configure(socketio(socketPort, callback));
     const socket = io('http://localhost:' + socketPort);
 
     it('should be connected', (done) => {
-      const server = app.listen(appPort);
-      app.setup(server);
-      server.on('listening', () => {
+      serverListen(app, appPort, () => {
         socket.once('event', done);
       });
     });
   });
 
   describe('Pass port, options and callback', function () {
-    const socketPort = getUniquePort();
-    const appPort = getUniquePort();
+    const socketPort = createUniquePort();
+    const appPort = createUniquePort();
     const app = feathers().configure(socketio(socketPort, options, callback));
     const socket = io('http://localhost:' + socketPort, options);
 
     it('should be connected', (done) => {
-      const server = app.listen(appPort);
-      app.setup(server);
-      server.on('listening', () => {
+      serverListen(app, appPort, () => {
         socket.once('event', done);
       });
     });
   });
 });
+
+function UniquePortCreator (from) {
+  return () => from++;
+}
+
+function serverListen (app, port, cb) {
+  let server = app.listen(port);
+  app.setup(server);
+  server.on('listening', cb);
+}
